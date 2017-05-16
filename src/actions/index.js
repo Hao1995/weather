91d02d32516f169bf13a28c4e9096f6d378c9649
Hao Yu <y26704325@gmail.com>
2017-05-16 04:52:04 +0000

    submit a request for weather API

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_KEY ="84abbd9251a97863c67dd475c7b60022";
-const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = "FETCH_WEATHER";
 
